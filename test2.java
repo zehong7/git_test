711d4ba0641ff301738e3345b4fef2ddb0e07980
wei zehong <weizehong@lap.top>
2025-09-13 16:35:36 +0800

    change java file

--- test2.java
+++ test2.java
@@ -3,6 +3,6 @@ import java.util.*;
 import java.io.*;
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("adding sth!");
     }
 }
