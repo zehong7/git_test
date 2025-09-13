30bdeb0ce78f5dd5109dc9ec0d19ba0ec6af4d39
wei zehong <weizehong777@outlook.com>
2025-09-13 21:54:12 +0800

    error version

--- test2.java
+++ test2.java
@@ -4,5 +4,6 @@ import java.io.*;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("adding sth!");
+        System.out.println("This is an error, you need to return!");
     }
 }
