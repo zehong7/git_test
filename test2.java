21909229374c070fc3c2823f410f2fa7309218ff
wei zehong <weizehong@lap.top>
2025-09-13 16:30:06 +0800

    message for commit 1a2b3c4d5e6f7g8h9i0j

--- test2.java
+++ test2.java
@@ -1,3 +1,8 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
 public class test2 {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
